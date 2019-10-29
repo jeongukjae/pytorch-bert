4f86b6cf79dcac1c38f68a5f9ddd8fd9354acea0
jeongukjae <JeongUkJae@gmail.com>
2019-10-30 01:21:44 +0900

    add masked lm & nsp

--- pytorch_bert/cli.py
+++ pytorch_bert/cli.py
@@ -5,7 +5,7 @@ import torch
 
 from .feature import FeatureExtractor
 from .modeling import Bert, BertConfig, BertMLM, BertNSP
-from .tokenizer import SubWordTokenizer, Vocab
+from .tokenizer import SubWordTokenizer, SpecialToken, Vocab
 from .weight_converter import WeightConverter
 
 parser = ArgumentParser("bert")
@@ -27,6 +27,12 @@ masked_lm_parser.add_argument('-m', '--model', required=True, dest='model_path',
 masked_lm_parser.add_argument('--head', required=True, dest='masked_lm_head_path', help='A converted model that contains Masked LM Head weight')
 
 nsp_parser = subparsers.add_parser('nsp', help ='A command that tests mask lm using pretrained model')
+nsp_parser.add_argument('first', help='A first one of sequence pair')
+nsp_parser.add_argument('second', help='A second one of sequence pair')
+nsp_parser.add_argument('-c', '--config', required=True, dest='config_path', help='A configuration file path of pretrained model')
+nsp_parser.add_argument('-v', '--vocab', required=True, dest='vocab_path', help='A vocab file path')
+nsp_parser.add_argument('-m', '--model', required=True, dest='model_path', help='A converted model that contains Bert weight')
+nsp_parser.add_argument('--head', required=True, dest='nsp_head_path', help='A converted model that contains NSP Head weight')
 
 
 def main():
@@ -67,13 +73,8 @@ def _convert_tf_weight_to_pytorch(args):
 def _test_masked_lm(args):
     config = BertConfig.from_json(args.config_path)
 
-    bert = Bert(config)
-    bert.load_state_dict(torch.load(args.model_path))
-    bert.eval()
-
-    mlm_head = BertMLM(config)
-    mlm_head.load_state_dict(torch.load(args.masked_lm_head_path))
-    mlm_head.eval()
+    bert = _load_model(Bert(config), args.model_path)
+    mlm_head = _load_model(BertMLM(config), args.masked_lm_head_path)
 
     vocab = Vocab(args.vocab_path)
     tokenizer = SubWordTokenizer(vocab)
@@ -83,18 +84,71 @@ def _test_masked_lm(args):
     if args.second:
         sequence_pair += (args.second,)
 
-    tokens, input_type_ids, input_ids, input_mask = feature_extractor.convert_sequences_to_feature(sequence_pair, config.max_position_embeddings)
-    print(f"parsed tokens: {tokens}")
+    feature = feature_extractor.convert_sequences_to_feature(sequence_pair, config.max_position_embeddings)
+    feature, masked_labels = feature_extractor.mask(feature, vocab.vocab[SpecialToken.mask])
 
-    # mask = torch.ones((1, 512), dtype=torch.bool) ^ torch.tensor(input_mask, dtype=torch.bool).unsqueeze(0)
-    # encoder_outputs, _ = bert(torch.tensor([input_ids]), torch.tensor([input_type_ids]), mask)
-    # mlm_output = mlm_head(encoder_outputs)
+    input_ids = torch.tensor([feature.input_ids])
+    input_type_ids = torch.tensor([feature.input_type_ids])
+    input_mask = feature_extractor.create_input_mask(feature.input_mask, config.max_position_embeddings)
+
+    print("")
+    print(f'input text: {sequence_pair}')
+    print(f"parsed tokens: {feature.tokens}")
+    print("")
+
+    encoder_outputs, _ = bert(input_ids, input_type_ids, input_mask)
+    mlm_output = mlm_head(encoder_outputs)
+
+    for index, pos in enumerate(masked_labels.positions):
+        predicted = torch.argmax(mlm_output[pos,:,:], -1).item()
+        predicted = vocab.inv_vocab[predicted]
+        answer = vocab.inv_vocab[masked_labels.answers[index]]
+        print(f'masked pos {pos}. answer: {answer}, predicted: {predicted}')
+
 
 def _test_nsp(args):
-    pass
+    config = BertConfig.from_json(args.config_path)
+
+    bert = _load_model(Bert(config), args.model_path)
+    nsp_head = _load_model(BertNSP(config), args.nsp_head_path)
+
+    vocab = Vocab(args.vocab_path)
+    tokenizer = SubWordTokenizer(vocab)
+    feature_extractor = FeatureExtractor(tokenizer)
+
+    sequence_pair = (args.first,args.second)
+
+    feature = feature_extractor.convert_sequences_to_feature(sequence_pair, config.max_position_embeddings)
+    feature, _ = feature_extractor.mask(feature, vocab.vocab[SpecialToken.mask])
+
+    input_ids = torch.tensor([feature.input_ids])
+    input_type_ids = torch.tensor([feature.input_type_ids])
+    input_mask = feature_extractor.create_input_mask(feature.input_mask, config.max_position_embeddings)
+
+    print("")
+    print(f'input text: {sequence_pair}')
+    print(f"parsed tokens: {feature.tokens}")
+    print("")
+
+    _, pooled_output = bert(input_ids, input_type_ids, input_mask)
+    nsp_output = nsp_head(pooled_output)
+
+    print(f'nsp : {"IsNext" if torch.argmax(nsp_output).item() == 0 else "NotNext"}')
+
+
 
 def _save_model(model: torch.nn.Module, path: str, is_full: bool):
     if is_full:
         torch.save(model, path)
     else:
         torch.save(model.state_dict(), path)
+
+def _load_model(model: torch.nn.Module, path: str, is_full: bool = False):
+    if is_full:
+        model = torch.load(path)
+        model.eval()
+    else:
+        model.load_state_dict(torch.load(path))
+        model.eval()
+
+    return model
