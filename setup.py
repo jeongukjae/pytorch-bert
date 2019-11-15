47d04adf8b6ba356da06908418c2a5ed67b5dd31
JeongUkJae <jeongukjae@gmail.com>
2019-11-15 21:35:04 +0900

    add classifiers

--- setup.py
+++ setup.py
@@ -6,10 +6,19 @@ setup(
     install_requires=["torch>=1.3.0"],
     extras_require={"with-tf": ["tensorflow>=2.0.0", "numpy"]},
     packages=find_packages(exclude=["tests"]),
-
+    python_requires=">=3.6, <3.8",
+    #
     description="bert implementation",
     author="Jeong Ukjae",
     author_email="jeongukjae@gmail.com",
     url="https://github.com/jeongukjae/pytorch-bert",
-    python_requires=">=3.6, <3.8",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
