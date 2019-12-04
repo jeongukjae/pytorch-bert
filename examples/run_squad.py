fd31561b7106ad3e86b2bc49b2c0f159cbc0d814
JeongUkJae <jeongukjae@gmail.com>
2019-12-04 23:48:04 +0900

    remove utils for squad

--- examples/run_squad.py
+++ examples/run_squad.py
@@ -1,13 +1,32 @@
 # WIP
-import os
-from multiprocessing import Pool
+import json
+import argparse
+import logging
+import sys
+from typing import NamedTuple, List
 
 import torch
 from torch import nn
+from torch.utils.data import TensorDataset, RandomSampler, DataLoader
 
 from pytorch_bert import Bert, BertConfig, SubWordTokenizer
+from pytorch_bert.tokenizer import clean_text
 from pytorch_bert.weight_converter import load_tf_weight_to_pytorch_bert
-from util_for_squad import download_model_file, prepare_dataset, read_squad_example
+from pytorch_bert.feature import convert_sequences_to_feature
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model-path", required=True)
+parser.add_argument("--config-path", required=True)
+parser.add_argument("--vocab-path", required=True)
+parser.add_argument("--squad-train-path", required=True)
+parser.add_argument("--batch-size", default=32, type=int)
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
+logger.addHandler(handler)
 
 
 class BertForSquad(nn.Module):
@@ -16,44 +35,70 @@ class BertForSquad(nn.Module):
         self.bert = Bert(config)
         self.squad_layer = nn.Linear(config.hidden_size, 2)
 
-        nn.init.normal_(self.squad_layer.weight, std=0.02)
-        nn.init.zeros_(self.squad_layer.bias)
-
     def forward(self, input_ids: torch.Tensor, token_type_ids: torch.Tensor, attention_mask: torch.Tensor):
         encoder_outputs, _ = self.bert(input_ids, token_type_ids, attention_mask)
-        encoder_outputs = encoder_outputs.view(-1, config.hidden_size)
-
         logits = self.squad_layer(encoder_outputs)
         return logits
 
 
-def get_index(logit: torch.Tensor):
-    return logit.argsort(dim=0)
+class SquadExample(NamedTuple):
+    context_text: str
+    question_text: str
+    answer_text: str
+    start_position: int
+    end_position: int
+
+
+def main():
+    args = parser.parse_args()
+
+    logger.info(f"initialize model and converting weight from {args.model_path}")
+    config = BertConfig.from_json(args.config_path)
+    model = BertForSquad(config)
+    load_tf_weight_to_pytorch_bert(model.bert, config, args.model_path)
+
+    logger.info(f"initialize tokenizer using vocab {args.vocab_path}")
+    tokenizer = SubWordTokenizer(args.vocab_path)
+
+    logger.info(f"read squad dataset from {args.squad_train_path}")
+    tokenizer = SubWordTokenizer(args.vocab_path)
+    with open(args.squad_train_path, "r") as f:
+        paragraphs = [paragraph for data in json.load(f)["data"] for paragraph in data["paragraphs"]]
+
+    logger.info(f"convert squad dataset to features")
+    examples: List[SquadExample] = []
+    for paragraph in paragraphs:
+        original_context_text = paragraph["context"]
+        context_text = clean_text(original_context_text)
+
+        for qa in paragraph["qas"]:
+            question_text = clean_text(qa["question"])
+            answer = qa["answers"][0]
+            answer_text = clean_text(answer["text"])
+
+            start_position = context_text[: context_text.index(answer_text)].count(" ")
+            end_position = start_position + answer_text.count(" ")
+
+            examples.append(SquadExample(context_text, question_text, answer_text, start_position, end_position))
+
+    features = [
+        convert_sequences_to_feature(
+            tokenizer, (example.context_text, example.question_text), config.max_position_embeddings
+        )
+        for example in examples
+    ]
+
+    logger.info(f"create dataloader from squad features")
+    input_ids = torch.tensor([feature.input_ids for feature in features])
+    input_type_ids = torch.tensor([feature.input_type_ids for feature in features])
+    input_mask = torch.tensor([feature.input_mask for feature in features])
+    start_positions = torch.tensor([example.start_position for example in examples])
+    end_positions = torch.tensor([example.end_position for example in examples])
+
+    dataset = TensorDataset(input_type_ids, input_ids, input_mask, start_positions, end_positions)
+    sampler = RandomSampler(dataset)
+    train_loader = DataLoader(dataset, sampler=sampler, batch_size=args.batch_size)
 
 
 if __name__ == "__main__":
-    num_processes = int(os.getenv("PYTORCH_BERT_NUM_PROCESSES", 8))
-    pool = Pool(num_processes)
-
-    print("start to download model file")
-    download_model_file(
-        "https://storage.googleapis.com/bert_models/2018_11_23/multi_cased_L-12_H-768_A-12.zip",
-        "/tmp/bert-base",
-        force_download=False,
-    )
-
-    print("build BERT model")
-    config = BertConfig.from_json("/tmp/bert-base/multi_cased_L-12_H-768_A-12/bert_config.json")
-    model = BertForSquad(config)
-
-    print("load BERT weight")
-    load_tf_weight_to_pytorch_bert(model.bert, config, "/tmp/bert-base/multi_cased_L-12_H-768_A-12/bert_model.ckpt")
-
-    print("prepare dataset")
-    tokenizer = SubWordTokenizer("/tmp/bert-base/multi_cased_L-12_H-768_A-12/vocab.txt")
-
-    print("read squad file")
-    examples = read_squad_example("./data/train-v1.1.json", pool)
-
-    print("convert squad example to dataset")
-    dataset = prepare_dataset(examples, tokenizer, config.max_position_embeddings, pool)
+    main()
